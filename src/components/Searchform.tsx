23f1e98407b4cace44933f9dfc31a70ce6d92cc6
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2023-09-01 23:08:33 +0200

    a

--- src/components/Searchform.tsx
+++ src/components/Searchform.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useFormik } from 'formik';
 import Grid from '@mui/material/Unstable_Grid2';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
@@ -12,7 +13,73 @@ const db = getFirestore();
 export default function Searchform() {
   const [user, setUser] = React.useState<User | null>(null);
   const [matchesData, setMatchesData] = React.useState<any[]>([]);
-  const [number, setNumber] = React.useState('10');
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      numberOfMatches: '10',
+    },
+    onSubmit: async (values) => {
+      const numberOfMatches = parseInt(values.numberOfMatches);
+    
+      if (numberOfMatches === 0 || numberOfMatches > 10) {
+        toast.error("Il numero di partite deve essere compreso tra 1 e 10.")
+        return;
+      }
+    
+      if (!/^\d{7,10}$/.test(values.username)) {
+        toast.error("Per favore, inserisci un numero tra 7 e 10 cifre.")
+        return;
+      }
+    
+      if (user && user.displayName) {
+        const userRef = doc(db, 'users', user.displayName);
+        const docSnap = await getDoc(userRef);
+    
+        if (docSnap.exists()) {
+          const currentQueries = docSnap.data().numberOfQueries;
+          await setDoc(userRef, {
+            numberOfQueries: (currentQueries || 0) + 1,
+          }, { merge: true });
+        } else {
+          await setDoc(userRef, {
+            numberOfQueries: 1,
+          });
+        }
+    
+        const matchesRef = doc(db, 'matches', values.username);
+        const matchesDocSnap = await getDoc(matchesRef);
+    
+        if (matchesDocSnap.exists()) {
+          const matchesData = matchesDocSnap.data().matchesData;
+          setMatchesData(matchesData.slice(0, parseInt(values.numberOfMatches)));
+          toast.success('Caricamento Firebase');
+      } else {
+          toast('Caricamento API...')
+          fetch(`https://api.opendota.com/api/players/${values.username}/matches`)
+              .then(response => response.json())
+              .then(data => {
+                  if (!data || data.length === 0 || data.error) {
+                      toast.error("ID non valido");
+                      return;
+                  }
+                  const last50Matches = data.slice(0, 51);
+                  const matchesData = last50Matches;
+                  // Controllo se i dati recuperati dalla chiamata API sono diversi da quelli già memorizzati nel Firestore
+                  if (JSON.stringify(matchesData) !== JSON.stringify(matchesDocSnap.data()?.matchesData)) {
+                      setDoc(matchesRef, { matchesData }, { merge: true });
+                  }
+                  setMatchesData(matchesData.slice(0, parseInt(values.numberOfMatches)));
+                  toast.success('Caricamento API');
+              })
+              .catch(error => {
+                  console.error('Errore:', error);
+                  toast.error("Non valido");
+              });
+        }
+      }
+    },
+  });
 
   React.useEffect(() => {
     const auth = getAuth();
@@ -23,82 +90,28 @@ export default function Searchform() {
     return () => unsubscribe();
   }, []);
 
-  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const inputElement = event.currentTarget.querySelector('#outlined-basic') as HTMLInputElement;
-    const numberOfMatches = parseInt(number);
-
-    if (numberOfMatches === 0 || numberOfMatches > 10) {
-      toast.error("Il numero di partite deve essere compreso tra 1 e 10.")
-      return;
-    }
-  
-    if (!inputElement || !/^\d{7,10}$/.test(inputElement.value)) {
-      toast.error("Per favore, inserisci un numero tra 7 e 10 cifre.")
-      return;
-    }
-
-    if (user && user.displayName) {
-      const userRef = doc(db, 'users', user.displayName);
-      const docSnap = await getDoc(userRef);
-
-      if (docSnap.exists()) {
-        const currentQueries = docSnap.data().numberOfQueries;
-        await setDoc(userRef, {
-          numberOfQueries: (currentQueries || 0) + 1,
-        }, { merge: true });
-      } else {
-        await setDoc(userRef, {
-          numberOfQueries: 1,
-        });
-      }
-
-      toast.loading('Caricamento in corso...')
-      fetch(`https://api.opendota.com/api/players/${inputElement.value}/matches`)
-      .then(response => response.json())
-      .then(data => {
-        if (!data || data.length === 0 || data.error) { // controlla se la risposta � vuota, ha lunghezza zero o contiene un errore
-          toast.error("Non valido");
-          return;
-        }
-        const last50Matches = data.slice(0, 51);
-        const matchesData = last50Matches;
-        const matchesRef = doc(db, 'matches', inputElement.value);
-        setDoc(matchesRef, { matchesData }, { merge: true });
-        setMatchesData(matchesData.slice(0, parseInt(number)));
-        toast.success('Caricamento completato');
-      })
-      .catch(error => {
-        console.error('Errore:', error);
-        toast.error("Non valido");
-      });
-
-    }
-  };
-
-  const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNumber(event.target.value);
-  };
-
   return (
     <Grid container>
       <Grid xs={12}>
-        <form onSubmit={handleSearch}>
+        <form onSubmit={formik.handleSubmit}>
           <TextField
-            id="outlined-basic"
+            id="username"
+            name="username"
             label="Nome utente"
             variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.username}
           />
           <TextField
-            id="number"
+            id="numberOfMatches"
+            name="numberOfMatches"
             label="Numero di partite"
             variant="outlined"
-            value={number}
-            onChange={handleNumberChange}
+            onChange={formik.handleChange}
+            value={formik.values.numberOfMatches}
           />
           <IconButton aria-label="delete" type="submit">
-            <SearchIcon/>
+            <SearchIcon />
             <Toaster />
           </IconButton>
         </form>
@@ -107,13 +120,14 @@ export default function Searchform() {
       {matchesData.map((match, index) => (
         <Grid xs={12} key={index} alignItems="left" justifyContent="left" textAlign="left">
           <h2>Partita {index + 1}</h2>
-          <p>Assists: {match.assists}</p>
-          <p>Morte: {match.deaths}</p>
-          <p>Durata: {match.duration}</p>
-          <p>Hero ID: {match.hero_id}</p>
-          <p>Kills: {match.kills}</p>
+          <p>🗡️ Kills: {match.kills}</p>
+          <p>🙏 Assists: {match.assists}</p>
+          <p>☠️ Morte: {match.deaths}</p>
+          <p>⏰️ Durata: {match.duration}</p>
+          <p>🆔 Hero ID: {match.hero_id}</p>
         </Grid>
       ))}
     </Grid>
   );
 }
+
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,73 +1,80 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Unstable_Grid2';
 import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import toast, { Toaster } from 'react-hot-toast';
 
 function Signup() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const auth = getAuth();
   const db = getFirestore();
 
-  const handleSignUp = async () => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (name.length === 0) {
-      alert('Inserisci il tuo nome.');
-      return;
-    }
-    if (!emailPattern.test(email)) {
-      alert('Inserisci un indirizzo email valido.');
-      return;
-    }
-    if (password.length < 6) {
-      alert('La password deve essere lunga almeno 6 caratteri');
-      return;
-    }
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      console.log('Utente registrato con successo:', user);
-      toast.success('Utente registrato con successo');
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (values.name.length === 0) {
+        alert('Inserisci il tuo nome.');
+        return;
+      }
+      if (!emailPattern.test(values.email)) {
+        alert('Inserisci un indirizzo email valido.');
+        return;
+      }
+      if (values.password.length < 6) {
+        alert('La password deve essere lunga almeno 6 caratteri');
+        return;
+      }
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, values.email, values.password);
+        const user = userCredential.user;
+        console.log('Utente registrato con successo:', user);
+        toast.success('Utente registrato con successo');
 
-      // Aggiorna il profilo dell'utente
-      await updateProfile(user, { displayName: name });
-      console.log('Profilo aggiornato:', user);
+        // Aggiorna il profilo dell'utente
+        await updateProfile(user, { displayName: values.name });
+        console.log('Profilo aggiornato:', user);
 
-      // Salvare il nome e l'email su Firestore
-      const docRef = doc(db, "users", name);
-      await setDoc(docRef, {
-          name: name,
-          email: email,
-      });
-      console.log("Document written with ID: ", docRef.id);
+        // Salvare il nome e l'email su Firestore
+        const docRef = doc(db, "users", values.name);
+        await setDoc(docRef, {
+            name: values.name,
+            email: values.email,
+        });
+        console.log("Document written with ID: ", docRef.id);
 
-      window.location.href = '/';
-    } catch (error) {
-      console.error('Errore durante la registrazione:', error);
-    }
-  }
+        window.location.href = '/';
+      } catch (error) {
+        console.error('Errore durante la registrazione:', error);
+      }
+    },
+  });
 
   return (
     <Grid container spacing={2} alignItems="center" justifyContent="center" style={{ paddingTop: '1%' }}>
-      <Grid>
-        <TextField id="outlined-name" label="Nome" variant="outlined" value={name} onChange={e => setName(e.target.value)}/>
-      </Grid>
-      <Grid>
-        <TextField id="outlined-email" label="Email" variant="outlined" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
-      </Grid>
-      <Grid>
-        <TextField id="outlined-password" label="Password" variant="outlined" type="password" value={password} onChange={e => setPassword(e.target.value)}/>
-      </Grid>
-      <Grid>
-        <IconButton aria-label="signup" onClick={handleSignUp}>
-          <SearchIcon />
-        </IconButton>
-      </Grid>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid>
+          <TextField id="outlined-name" label="Nome" variant="outlined" value={formik.values.name} onChange={formik.handleChange} name="name"/>
+        </Grid>
+        <Grid>
+          <TextField id="outlined-email" label="Email" variant="outlined" type="email" value={formik.values.email} onChange={formik.handleChange} name="email"/>
+        </Grid>
+        <Grid>
+          <TextField id="outlined-password" label="Password" variant="outlined" type="password" value={formik.values.password} onChange={formik.handleChange} name="password"/>
+        </Grid>
+        <Grid>
+          <IconButton aria-label="signup" type="submit">
+            <SendIcon />
+          </IconButton>
+        </Grid>
+      </form>
       <Toaster />
     </Grid>
   )
